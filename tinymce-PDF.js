014954c99d3d10a1b54887a5c25bf1ae67dffa17
bad14 <rigohvz14@gmail.com>
2021-01-22 16:39:18 -0600

    Update tinymce-PDF.js

--- tinymce-PDF.js
+++ tinymce-PDF.js
@@ -122,3 +122,5 @@ jQuery(document).ready(function ($) {
 	  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
     });
     )};
+		       
+		       ///credits to https://gist.github.com/muhadmr/89f4ac92b3cca2a2df4f466947e0332d
